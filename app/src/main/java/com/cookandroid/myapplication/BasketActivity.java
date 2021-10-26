5010fa2454d35b38c3b545f6223afbdb8a101a15
구본유 <adidas1997@naver.com>
2021-10-26 21:46:11 +0900

    bootpay module BasketActivity , FoodDetailActivity build.gradle (project,module)

--- app/src/main/java/com/cookandroid/myapplication/BasketActivity.java
+++ app/src/main/java/com/cookandroid/myapplication/BasketActivity.java
@@ -1,6 +1,8 @@
 package com.cookandroid.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +16,19 @@ import android.widget.TimePicker;
 
 import java.util.ArrayList;
 
+import kr.co.bootpay.Bootpay;
+import kr.co.bootpay.BootpayAnalytics;
+import kr.co.bootpay.enums.Method;
+import kr.co.bootpay.enums.PG;
+import kr.co.bootpay.enums.UX;
+import kr.co.bootpay.listener.CancelListener;
+import kr.co.bootpay.listener.CloseListener;
+import kr.co.bootpay.listener.ConfirmListener;
+import kr.co.bootpay.listener.DoneListener;
+import kr.co.bootpay.listener.ReadyListener;
+import kr.co.bootpay.model.BootExtra;
+import kr.co.bootpay.model.BootUser;
+
 public class BasketActivity extends AppCompatActivity {
 
     TimePicker timePicker;
@@ -26,12 +41,14 @@ public class BasketActivity extends AppCompatActivity {
     private Food food;
     private int totalPrice;
     private TextView tv_totalPrice;
-
+    private int stuck = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket);
+
+        BootpayAnalytics.init(this, "616530847b5ba4002152c403");
 
         totalPrice = 0;
 
@@ -56,6 +73,11 @@ public class BasketActivity extends AppCompatActivity {
         btnResv = (RadioButton) findViewById(R.id.radioButton_resv);
         order = (Button) findViewById(R.id.button_order);
 
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.constraintLayout2);
+
+        if(totalPrice == 0)
+            layout.setVisibility(View.INVISIBLE);
+
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +90,62 @@ public class BasketActivity extends AppCompatActivity {
                     task.execute("http://"+DB.getIP()+"/order.php",postParameters);
                 }
                 DB.resetFoodList_Basket();
-                finish();
+
+/*
+결제 모듈
+  */
+                BootUser bootUser = new BootUser().setPhone("010-1234-5678"); // !! 자신의 핸드폰 번호로 바꾸기
+                BootExtra bootExtra = new BootExtra().setQuotas(new int[] {0, 2, 3});
+
+                Bootpay.init(getFragmentManager())
+                        .setApplicationId("616530847b5ba4002152c403") // 해당 프로젝트(안드로이드)의 application id 값(위의 값 복붙)
+                        .setPG(PG.KCP) // 결제할 PG 사
+                        .setMethod(Method.CARD) // 결제수단
+                        .setContext(BasketActivity.this)
+                        .setBootUser(bootUser)
+                        .setBootExtra(bootExtra)
+                        .setUX(UX.PG_DIALOG)
+//                .setUserPhone("010-1234-5678") // 구매자 전화번호
+                        .setName("부경대 학식") // 결제할 상품명
+                        .setOrderId("1234") // 결제 고유번호 (expire_month)
+                        .setPrice(totalPrice) // 결제할 금액
+                        .onConfirm(new ConfirmListener() { // 결제가 진행되기 바로 직전 호출되는 함수로, 주로 재고처리 등의 로직이 수행
+                            @Override
+                            public void onConfirm(@Nullable String message) {
+
+                                if (0 < stuck) Bootpay.confirm(message); // 재고가 있을 경우.
+                                else Bootpay.removePaymentWindow(); // 재고가 없어 중간에 결제창을 닫고 싶을 경우
+                                Log.d("confirm", message);
+                            }
+                        })
+                        .onDone(new DoneListener() { // 결제완료시 호출, 아이템 지급 등 데이터 동기화 로직을 수행합니다
+                            @Override
+                            public void onDone(@Nullable String message) {
+                                Log.d("done", message);
+                            }
+                        })
+                        .onReady(new ReadyListener() { // 가상계좌 입금 계좌번호가 발급되면 호출되는 함수입니다.
+                            @Override
+                            public void onReady(@Nullable String message) {
+                                Log.d("ready", message);
+                            }
+                        })
+                        .onCancel(new CancelListener() { // 결제 취소시 호출
+                            @Override
+                            public void onCancel(@Nullable String message) {
+
+                                Log.d("cancel", message);
+                            }
+                        })
+                        .onClose(
+                                new CloseListener() { //결제창이 닫힐때 실행되는 부분
+                                    @Override
+                                    public void onClose(String message) {
+                                        Log.d("close", "close");
+                                    }
+                                })
+                        .request();
+
             }
         });
 
--- app/src/main/java/com/cookandroid/myapplication/FoodDetailActivity.java
+++ app/src/main/java/com/cookandroid/myapplication/FoodDetailActivity.java
@@ -1,5 +1,6 @@
 package com.cookandroid.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -12,9 +13,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import kr.co.bootpay.Bootpay;
+import kr.co.bootpay.enums.Method;
+import kr.co.bootpay.enums.PG;
+import kr.co.bootpay.enums.UX;
+import kr.co.bootpay.listener.CancelListener;
+import kr.co.bootpay.listener.CloseListener;
+import kr.co.bootpay.listener.ConfirmListener;
+import kr.co.bootpay.listener.DoneListener;
+import kr.co.bootpay.listener.ReadyListener;
+import kr.co.bootpay.model.BootExtra;
+import kr.co.bootpay.model.BootUser;
+
 public class FoodDetailActivity extends AppCompatActivity {
 
-    Button btn_addbasket, btn_plus, btn_minus;
+    private int stuck =10;
+    Button btn_addbasket, btn_plus, btn_minus, btn_order;
     ImageView img_wish;
     TextView tv_foodname, tv_count, tv_foodinfo;
     Food selectfood;
@@ -29,6 +43,8 @@ public class FoodDetailActivity extends AppCompatActivity {
         btn_addbasket = (Button) findViewById(R.id.addbasket);
         btn_plus = (Button) findViewById(R.id.plus);
         btn_minus = (Button) findViewById(R.id.minus);
+        btn_order =  (Button) findViewById(R.id.button_order2);
+
         img_wish = (ImageView) findViewById(R.id.wish);
 
         tv_foodname = (TextView) findViewById(R.id.foodname);
@@ -53,6 +69,7 @@ public class FoodDetailActivity extends AppCompatActivity {
 
 
         count =1;
+
 
         btn_addbasket.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,7 +130,73 @@ public class FoodDetailActivity extends AppCompatActivity {
             }
         });
 
+        btn_order.setOnClickListener( new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+
+                        BootUser bootUser = new BootUser().setPhone("010-1234-5678"); // !! 자신의 핸드폰 번호로 바꾸기
+                        BootExtra bootExtra = new BootExtra().setQuotas(new int[] {0, 2, 3});
+
+                        Bootpay.init(getFragmentManager())
+                                .setApplicationId("616530847b5ba4002152c403") // 해당 프로젝트(안드로이드)의 application id 값(위의 값 복붙)
+                                .setPG(PG.KCP) // 결제할 PG 사
+                                .setMethod(Method.CARD) // 결제수단
+                                .setContext(FoodDetailActivity.this)
+                                .setBootUser(bootUser)
+                                .setBootExtra(bootExtra)
+                                .setUX(UX.PG_DIALOG)
+//                .setUserPhone("010-1234-5678") // 구매자 전화번호
+                                .setName("부경대 학식") // 결제할 상품명
+                                .setOrderId("1234") // 결제 고유번호 (expire_month)
+                                .setPrice(selectfood.getFoodPrice()) // 결제할 금액
+                                .onConfirm(new ConfirmListener() { // 결제가 진행되기 바로 직전 호출되는 함수로, 주로 재고처리 등의 로직이 수행
+                                    @Override
+                                    public void onConfirm(@Nullable String message) {
+
+                                        if (0 < stuck) Bootpay.confirm(message); // 재고가 있을 경우.
+                                        else Bootpay.removePaymentWindow(); // 재고가 없어 중간에 결제창을 닫고 싶을 경우
+                                        Log.d("confirm", message);
+                                    }
+                                })
+                                .onDone(new DoneListener() { // 결제완료시 호출, 아이템 지급 등 데이터 동기화 로직을 수행합니다
+                                    @Override
+                                    public void onDone(@Nullable String message) {
+                                        Log.d("done", message);
+                                    }
+                                })
+                                .onReady(new ReadyListener() { // 가상계좌 입금 계좌번호가 발급되면 호출되는 함수입니다.
+                                    @Override
+                                    public void onReady(@Nullable String message) {
+                                        Log.d("ready", message);
+                                    }
+                                })
+                                .onCancel(new CancelListener() { // 결제 취소시 호출
+                                    @Override
+                                    public void onCancel(@Nullable String message) {
+
+                                        Log.d("cancel", message);
+                                    }
+                                })
+                                .onClose(
+                                        new CloseListener() { //결제창이 닫힐때 실행되는 부분
+                                            @Override
+                                            public void onClose(String message) {
+                                                Log.d("close", "close");
+                                            }
+                                        })
+                                .request();
+
+
+
+
+
+
+                    }
+                });
+
     }
+
     public boolean onOptionItemSelected(MenuItem menuItem)
     {
         switch (menuItem.getItemId()) {
